8c29815b115fe67cc35dd12934e85fcd9a9530c1
Jayson Perkins <jaysonperkins51@gmail.com>
2020-08-09 22:45:19 -0400

    Updates

---
+++ precache-manifest.259d2d245e518726a9b6e9bef26fe220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c40491b737cbaae02e892e6300dbdda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd29107666e2135e284",
+    "url": "/static/css/main.7c8505e5.chunk.css"
+  },
+  {
+    "revision": "22bae4169c750f488f7a",
+    "url": "/static/js/2.06abaa3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06abaa3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd29107666e2135e284",
+    "url": "/static/js/main.7abf6040.chunk.js"
+  },
+  {
+    "revision": "6fd889b72e3ebbbf40d3",
+    "url": "/static/js/runtime-main.c3a5cf1d.js"
+  }
+]);
